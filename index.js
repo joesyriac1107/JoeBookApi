4bcd1f256b3fc20195e70aaf55c14372bc22623e
Joe Syriac <joesyriac1107@gmail.com>
2021-07-08 19:07:52 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv= require("dotenv")
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+app.listen(8080,()=>{
+    console.log("Backend serever is running!fafafads")
+})
