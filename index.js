b5fc7e30f9fcad4eba8d585bd68fa31af01c29ba
josyriac <josyriac@microsoft.com>
2021-07-17 08:42:32 +0530

    register and login changes

--- index.js
+++ index.js
@@ -4,7 +4,52 @@ const mongoose = require("mongoose")
 const dotenv= require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
-app.listen(8080,()=>{
+const run = async () => {
+
+dotenv.config()
+mongConnect = () => new Promise((resolve,reject) => {
+    try {
+    mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+        console.log("Error: "+err)
+        if(!err)
+            resolve()
+    });
+} catch(err){
+    reject(err)
+}
+}) 
+
+try{
+    await mongConnect()
+    console.log("DB connection established")
+}catch(err){
+    console.log("DB connection failed")
+}
+//middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
+
+app.get("/",(req,res) => {
+    res.send("welcome to homepage")
+})
+
+app.get("/users",(req,res) => {
+    res.send("welcome to user homepage")
+})
+
+
+app.listen(8800,()=>{
     console.log("Backend serever is running!fafafads")
 })
+
+}
+
+run()
---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const router = require("express").Router()
+const User = require("../modals/user")
+const  bcrypt = require("bcrypt")
+
+//Register
+router.post("/register", async (req,res)=>{
+    console.log("body: "+req.body)
+
+   try {
+
+    //hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    
+    //create new user
+    const newUser = new User({
+        userName: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+    })
+
+    //save user to DB
+    const user = await newUser.save()
+    res.status(200).json(user)
+   }catch(err){
+    console.log(err)
+   }
+
+})
+
+//LOGIN
+router.post("/login", async (req,res) => {
+
+    try{
+    const user = await User.findOne({email: req.body.email})
+    !user && res.status(404).send("User not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.send(400).send("Invalid Credentials")
+    }catch(err){
+        console.log(err)
+    }
+})
+
+module.exports = router
