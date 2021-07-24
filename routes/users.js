b3827f53b4c0be068bed9f87f027d3e08735c910
Joe Syriac <joesyriac1107@gmail.com>
2021-07-24 17:13:55 +0530

    Delete method

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.get("/", (req,res)=>{
 //update user
 router.put("/:id", async (req,res) => {
     const modifiedUserDetails = req.body
-    if(req.body.userId == req.params.id || req.user.isAdmin){
+    if(req.body.userId == req.params.id || req.body.isAdmin){
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
@@ -30,6 +30,20 @@ router.put("/:id", async (req,res) => {
     }
 } )
 //delete user
+
+router.delete("/:id", async (req,res) => {
+    const modifiedUserDetails = req.body
+    if(req.body.userId == req.params.id || req.body.isAdmin){
+        try{
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("Account has been successfully deleted ")
+        }catch(err){
+            return res.status(500).json(err)
+        }
+    }else{
+        return res.status(403).json("You can delete only your account")
+    }
+} )
 //get a user
 //follow user
 //unfollow a user
