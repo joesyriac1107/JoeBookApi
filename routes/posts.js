35d98af65079d26d21c72ee16d10e8c237223ac1
Joe Syriac <joesyriac1107@gmail.com>
2021-07-25 13:51:28 +0530

    Posts API

--- routes/posts.js
+++ routes/posts.js
@@ -1,65 +1,105 @@
 const router = require('express').Router()
 const Post = require('../modals/Posts')
+const User = require('../modals/user')
 
-//create a post
+// create a post
 router.post('/', async (req, res) => {
-  const newPost = new Post(req.body)
-  try {
-    const savedPost = await newPost.save()
-    res.status(200).json(savedPost)
-  } catch (err) {
+    const newPost = new Post(req.body)
+    try {
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+// update a post
+router.put('/:id', async (req, res) => {
+    if (req.body.id == req.params.id) {
+        try {
+            const post = await Post.findById(req.params.id);
+            if (post.userId === req.body.userId) {
+                post.updateOne({$set: req.body})
+                res.status(200).json('Post has been successfully updated')
+            } else {
+                return res.status(403).json("You can modify only your posts")
+            }
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    } else {
+        return res.status(403).json("This post can't be modified because the modified post identifier is different")
+    }
+})
+
+// delete a post
+router.delete('/:id', async (req, res) => {
+    if (req.body.id == req.params.id) {
+        try {
+            const post = await Post.findById(req.params.id);
+            if (post.userId === req.body.userId) {
+                const deletedPost = post.deleteOne()
+                res.status(200).json('Post has been successfully deleted ')
+            } else {
+                return res.status(403).json("You can delete only your posts")
+            }
+        } catch (err) {
+            return res.status(500).json(err)
+        }
+    } else {
+        return res.status(403).json("This post can't be modified because the modified post identifier is different")
+    }
+})
+
+// like a post
+router.put('/:id/like', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (! post.likes.includes(req.body.userId)) {
+            await post.updateOne({
+                $push: {
+                    likes: req.body.userId
+                }
+            })
+            res.status(200).json("The post has been liked")
+        }else{
+          await post.updateOne({
+            $pull: {
+              likes: req.body.userId
+            }
+          })
+
+          res.status(200).json("The post has been disliked")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+// get a post
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post)
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+})
+
+// get timeline posts
+router.get('/timeline/all', async (req,res)=>{
+  let postArray = []
+  try{
+    const currentUser = await User.findById(req.body.userId)
+    const userPosts = await Post.find({userId: currentUser._id})
+    const friendPosts = await Promise.all(
+      currentUser.followings.map(friendId => {
+        return Post.find({userId:friendId})
+      })
+    )
+
+    res.status(200).json(userPosts.concat(...friendPosts))
+  }catch(err){
     res.status(500).json(err)
   }
 })
-//update a post
-router.put('/:id', async (req, res) => {
-  if (req.body.id == req.params.id) {
-    try {
-      const user = await Post.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      })
-      res.status(200).json('Post has been successfully updated')
-    } catch (err) {
-      return res.status(500).json(err)
-    }
-  } else {
-    return res
-      .status(403)
-      .json(
-        "This post can't be modified because the modified post identifier is different"
-      )
-  }
-})
 
-//delete a post
-router.delete('/:id', async (req, res) => {
-  if (req.body.id == req.params.id) {
-    try {
-      const post = await Post.findByIdAndDelete(req.params.id)
-      res.status(200).json('Post has been successfully deleted ')
-    } catch (err) {
-      return res.status(500).json(err)
-    }
-  } else {
-    return res
-      .status(403)
-      .json(
-        "This post can't be modified because the modified post identifier is different"
-      )
-  }
-})
-
-//like a post
-
-// get a post
-router.get('/:id', async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id)
-    res.status(200).json(post)
-  } catch (err) {
-    return res.status(500).json(err)
-  }
-})
-
-//get timeline post
 module.exports = router
